36d630a0b9afc2de110c143f63ad255c9658c8fa
jecanart <jecanart@espol.edu.ec>
2023-08-30 17:40:04 -0500

    correccion

--- routes/rest_macota.js
+++ routes/rest_macota.js
@@ -26,7 +26,7 @@ router.get('/findByIdU/:Usuario_idUsuario/json',function(req, res,next){
   
   let idU = req.params.Usuario_idUsuario;
 
-  macota.findByPk(Usuario_idUsuario)
+  macota.findByPk(idU)
   .then(instancia => {if(instancia){
     res.status(200).json(instancia);
   }else{
